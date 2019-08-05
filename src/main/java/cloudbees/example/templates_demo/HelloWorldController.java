e81cbc346d4b0942549e5c5ab0664c930afff018
blue <nofarb@gmail.com>
2019-08-05 16:05:08 -0400

    second commit

---
+++ src/main/java/cloudbees/example/templates_demo/HelloWorldController.java
@@ -0,0 +1,16 @@
+package cloudbees.example.templates_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Java Code Geeks!";
+    }
+
+}
