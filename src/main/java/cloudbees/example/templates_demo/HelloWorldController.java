e09f5345221dc27de918095650f30839d6fec942
nofarb <nofarb@gmail.com>
2019-08-09 16:24:26 -0400

    Update HelloWorldController.java

--- src/main/java/cloudbees/example/templates_demo/HelloWorldController.java
+++ src/main/java/cloudbees/example/templates_demo/HelloWorldController.java
@@ -2,6 +2,9 @@ package cloudbees.example.templates_demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import java.text.*;
+
 
 
 @RestController
@@ -10,11 +13,20 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello Java Code Geeks!";
+        return "Hello Java Code Geeks!" + getDate;
     }
 
      @GetMapping("/")
     public String sayHelloToALL() {
-        return "Hello Java Code Geeks!";
+        return "Hello Java Code Geeks!" + getDate;
     }
-}
+    
+    
+    Public String getDate(){
+        
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return dateFormat.format(date); //2013/10/15 16:16:39
+
+
+    }
